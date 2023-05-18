a8b915786d9bd24bf5324ad2f6fa73756fdc4cbb
giovanigalego10 <131292855+giovanigalego10@users.noreply.github.com>
2023-05-18 16:39:49 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,90 @@
+/*var numero1 = 100;
+var numero2 = 20;
+
+var resultado = numero1 / numero2;
+
+//alert("o resultado é: " + resultado);
+
+
+ var curso = {
+    nome : "Programacao",
+    cargaHoraria: 200,
+    alunosPresentes:  [ "Thiago", "Luis", "Marcel", "Danilo" ]
+}
+
+console.log("No curso de " + curso.nome + "o aluno destaque é" + curso.alunosPresentes[2])
+
+var time = {
+    nome : "Real Madrid",
+    país : "Espanha",
+    estadio : "Santiago Bernabeu",
+    títulos : [ "14 Champions League", "34 la ligas","etc" ]
+
+}
+
+console.log(time.nome + " é um time da  " + time.país + " que joga no " + time.estadio + " e tem muitos titulos, como: " + time.títulos)
+
+var senha = "giovanigalego10"
+
+if (senha == "focado_no_lucro_157"){
+    console.log("LOGANDO NO SISTEMA")
+}
+
+
+else {
+    console.log("SENHA INCORRETA")
+}
+*/
+
+//var idade = prompt("Qual a sua idade?");
+
+//if (idade >= 18){
+    //alert("Maior de idade")
+//}
+
+//else {
+    //alert("Menor de idade")
+//}
+
+
+/*var peso = prompt("Qual o seu peso?")
+var altura = prompt("Qual a sua altura?")
+
+var imc = peso / (altura * altura)
+alert(imc) 
+
+if (imc <= 18.5){
+    alert("MAGREZA")
+}
+
+else if (imc < 24.9){
+    alert("NORMAL")
+}
+
+else if (imc < 29.9){
+    alert("SOBREPESO")
+}
+
+else if (imc < 39.9){
+    alert("OBESIDADE")
+}
+
+else if (imc > 40.0){
+    alert("OBESIDADE GRAVE")
+}
+
+
+var listaDeHobbys = ["Jogar futebol", "assistir videos no Tik Tok", "moda"];
+
+listaDeHobbys.forEach(Hobby => alert("Hobbys: " + Hobby 0,
+))
+
+
+var curso = {
+    nome: "Programação de Disp. Móveis",
+    conteudo: [ "HTML," "CSS", "JavaScript" ],
+    cargaHoraria: 200
+};
+
+curso.conteudo.forEach(materia => alert("No curso de" + curso.nome + " temos aula de: " + materia))
+*/
